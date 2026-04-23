e4dc7366da4d09eee2312e91fc27079a01cabd91
Frank <205842738+frankangel-dev@users.noreply.github.com>
2026-04-23 05:27:26 -0700

    implement data fetching for render and click exercises

--- src/exercises/lesson-07/FetchOnRender.jsx
+++ src/exercises/lesson-07/FetchOnRender.jsx
@@ -1,11 +1,48 @@
 import './Lesson07Styles.css';
+import { useEffect, useState } from 'react';
+import { getPosts } from './api.js';
 
 export default function FetchOnRender() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const data = await getPosts();
+        setPosts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  if (loading) return <p className="loading">Loading posts...</p>;
+
+  if (error) return <p className="error">{error}</p>;
+
   return (
     <div className="root">
       <h1 className="heading">Fetch list of posts on render</h1>
       <div className="content">
-        TODO: Replace me with fetched data when the component renders
+        <ul>
+          {posts.map(({ id, title, body }) => {
+            return (
+              <li key={id}>
+                <h2>{title}</h2>
+                <p>{body}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
