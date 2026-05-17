1aa94939c6dea17aece494ac70d34375912fb439
Frank <205842738+frankangel-dev@users.noreply.github.com>
2026-05-17 03:48:03 -0700

    implement routing, navigation, and product details view with dynamic paths

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,13 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const { pathname } = useLocation();
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>{pathname}</p>
+      <Link to={'/'}>Go Home</Link>
     </section>
   );
 }
